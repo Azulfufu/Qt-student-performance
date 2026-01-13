f497648c7745f0d3272a18930976b60a7a81c171
dengzhenhui11 <2797278538@qq.com>
2026-01-13 23:15:32 +0800

    完善数据库函数

--- student/dbmanager.cpp
+++ student/dbmanager.cpp
@@ -12,3 +12,30 @@ bool DBManager::initDB(const QString& dbPath)
     qInfo() << "数据库连接成功！";
     return true;
 }
+
+QSqlQuery DBManager::execQuery(const QString& sql)
+{
+    QSqlQuery query(m_db);
+    if (!query.exec(sql)) {
+        qCritical() << "查询失败：" << sql << " 错误：" << query.lastError().text();
+    }
+    return query;
+}
+
+bool DBManager::execNonQuery(const QString& sql)
+{
+    QSqlQuery query(m_db);
+    if (!query.exec(sql)) {
+        qCritical() << "执行失败：" << sql << " 错误：" << query.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+QString DBManager::encryptPassword(QString password)
+{
+    // MD5加密：输入明文密码，返回32位加密字符串
+    QByteArray byteArray = password.toUtf8();
+    byteArray = QCryptographicHash::hash(byteArray, QCryptographicHash::Md5);
+    return byteArray.toHex();
+}
