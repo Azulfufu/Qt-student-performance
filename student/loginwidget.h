75cfab2874dc1640d24d6baf53549edc014d00cd
dengzhenhui11 <2797278538@qq.com>
2026-01-13 23:17:27 +0800

    登录加入数据库支持

--- student/loginwidget.h
+++ student/loginwidget.h
@@ -2,7 +2,7 @@
 #define LOGINWIDGET_H
 
 #include <QWidget>
-
+#include "dbmanager.h"
 
 namespace Ui {
 class LoginWidget;
@@ -16,10 +16,18 @@ public:
     explicit LoginWidget(QWidget *parent = nullptr);
     ~LoginWidget();
 
+signals:
+    // 登录成功信号：传递用户类型+账号
+    void loginSuccess(QString userType, QString username);
+
+private slots:
+    void on_btnLogin_clicked();  // 登录按钮点击
+    void on_btnCancel_clicked(); // 取消按钮点击
 
 private:
     Ui::LoginWidget *ui;
-
+    // 验证账号密码
+    bool verifyUser(QString username, QString password);
 };
 
 #endif // LOGINWIDGET_H
