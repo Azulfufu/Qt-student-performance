19d38e80f18bcf7f6dc43e8f9ee125852405c5e0
dengzhenhui11 <2797278538@qq.com>
2026-01-14 00:36:13 +0800

    在模块增加载入数据到表格的功能

--- student/scoreinputwidget.cpp
+++ student/scoreinputwidget.cpp
@@ -63,3 +63,49 @@ void ScoreInputWidget::loadCourses()
     }
 }
 
+void ScoreInputWidget::on_btnLoadStudents_clicked()
+{
+    // 获取选中的班级名称
+    QString className = ui->cbxClass->currentText();
+    if (className.isEmpty()) {
+        QMessageBox::warning(this, "提示", "请选择班级！");
+        return;
+    }
+
+    // 查询该班级的所有学生（ID + 姓名）
+    QString sql = QString("SELECT student_id, student_name FROM students WHERE class_name = '%1' ORDER BY student_id").arg(className);
+    QSqlQuery query = DBManager::getInstance().execQuery(sql);
+
+    // 清空表格原有数据
+    ui->tableStudents->setRowCount(0);
+    // 设置表格列数和表头
+    ui->tableStudents->setColumnCount(3);
+    ui->tableStudents->setHorizontalHeaderLabels({"学生ID", "学生姓名", "成绩"});
+
+    // 遍历查询结果，填充表格
+    while (query.next()) {
+        int row = ui->tableStudents->rowCount();
+        ui->tableStudents->insertRow(row); // 新增一行
+
+        // 1. 学生ID列（只读）
+        QTableWidgetItem *itemId = new QTableWidgetItem(query.value(0).toString());
+        itemId->setFlags(itemId->flags() & ~Qt::ItemIsEditable); // 取消编辑权限
+        itemId->setTextAlignment(Qt::AlignCenter); // 居中显示
+        ui->tableStudents->setItem(row, 0, itemId);
+
+        // 2. 学生姓名列（只读）
+        QTableWidgetItem *itemName = new QTableWidgetItem(query.value(1).toString());
+        itemName->setFlags(itemName->flags() & ~Qt::ItemIsEditable);
+        itemName->setTextAlignment(Qt::AlignCenter);
+        ui->tableStudents->setItem(row, 1, itemName);
+
+        // 3. 成绩列（可编辑，默认值0）
+        QTableWidgetItem *itemScore = new QTableWidgetItem("0");
+        itemScore->setTextAlignment(Qt::AlignCenter);
+        ui->tableStudents->setItem(row, 2, itemScore);
+    }
+
+    if (ui->tableStudents->rowCount() == 0) {
+        QMessageBox::information(this, "提示", QString("【%1】暂无学生数据！").arg(className));
+    }
+}
